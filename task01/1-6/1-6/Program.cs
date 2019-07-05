bd1dc69006bcc0178b6c885d15d640216c171c83
gaaasik <32246971+gaaasik@users.noreply.github.com>
2019-07-05 17:21:49 +0300

    Add files via upload

---
+++ task01/1-6/1-6/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_6
+{
+    class Program
+    {
+        enum tracings { Bold = 1, Italic, Underline } //список начинается с 1 дальше +1
+
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo key;
+            int p1 = 0, p2 = 0, p3 = 0; //для проверки повторений
+            string br = "       ";      // отступы для красоты
+            Console.Write("параметры надписи: ");
+ 
+             again:     // меткф again 
+            
+            if ((p1==0)&&(p2==0)&&(p3==0))
+            { Console.WriteLine("none"); }
+            
+            Console.WriteLine();                                               //изначальное меню
+            Console.WriteLine("введите:");
+            
+            Console.WriteLine(br.PadLeft(2)+"1:" + tracings.Bold);          
+            Console.WriteLine(br.PadLeft(2) + "2:" + tracings.Italic);
+            Console.WriteLine(br.PadLeft(2) + "3:" + tracings.Underline);
+            Console.WriteLine(br.PadLeft(2) + "4:" + "Для выхода из программы");
+
+                
+
+            int c = int.Parse(Console.ReadLine());
+            switch (c)
+            {
+                case (int)tracings.Bold:
+                    Console.Write("параметры надписи: ");
+                    if (p2==1)                      // проверяем повторения  Italics
+                    Console.Write(tracings.Italic + " ");
+                    if (p3 == 1)                    // проверяем повторения  Underline в других case то же самое
+                        Console.Write(tracings.Underline + " "); //если р =1 то значит выводим если нет то нет ))
+                    if (p1==1)
+                    {
+                        p1 = 0;
+                        break;
+                    }
+                    p1 += 1;
+                    Console.WriteLine(tracings.Bold+" ");
+                    break;
+
+
+
+                case (int)tracings.Italic:
+                    Console.Write("параметры надписи: ");
+                    if (p1 == 1)
+                        Console.Write(tracings.Bold + " ");
+                    if (p3 == 1)
+                        Console.Write(tracings.Underline + " ");
+                    if (p2 == 1)
+                    {
+                        p2 = 0;
+                        break;
+                    }
+
+                    p2 += 1;
+                    Console.WriteLine(tracings.Italic + " ");
+                    break;
+
+
+
+
+                case (int)tracings.Underline:
+                    Console.Write("параметры надписи: ");
+
+                    if (p1 == 1)
+                        Console.Write(tracings.Bold + " ");
+                    if (p2 == 1)
+                        Console.Write(tracings.Italic + " ");
+                   
+                    if (p3 == 1)
+                    {
+                        p3 = 0;
+                        break;
+                    }
+
+                    p3 += 1;
+                    Console.WriteLine(tracings.Underline + " ");
+                    break;
+
+                    case 4 :                //выход из программы
+                    Environment.Exit(0);
+                    break;
+
+
+
+            }
+            
+            
+            goto again;
+           
+        }
+    }
+}
---
+++ task01/1-7/1-7/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_7
+{
+    class Program
+    {
+        static void Sort(int[] A) // сортировка методом пузырька
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                for (int j = 0; j < A.Length - 1; j++)
+                {
+                    if (A[j] > A[j + 1])
+                    {
+                        int z = A[j];
+                        A[j] = A[j + 1];
+                        A[j + 1] = z;
+                    }
+                }
+            }
+
+        }
+        static void Print(int[] A)// вывод массива на экран 
+
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write(A[i] + " ");
+            }
+        }
+
+
+
+         static void max(int[] A)
+        { int max = A[0];
+            for(int i = 0;i<A.Length;i++)
+            {
+                if (max < A[i])
+                    max = A[i];
+            }
+            Console.WriteLine("максимальный элемент = " + max);
+        }
+
+
+
+        static void min (int[] A)
+        {
+            int min = A[0];
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (min > A[i])
+                    min = A[i];
+            }
+            Console.WriteLine("минимальный элемент = " + min);
+        }
+
+
+
+
+        static void Main(string[] args)
+        {   again:
+            Console.Write("Введите размер массива = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            int[] arr = new int[n];
+            Random rand = new Random(); // рандомный массив от - 100 до 100
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(-100, 100);
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            max(arr);
+            min(arr);
+            
+            Sort(arr);  // вызов функции
+            Console.WriteLine();
+            Console.WriteLine("Отсортированный масиив:");
+            Console.WriteLine();
+            Print(arr);
+            Console.WriteLine();
+
+
+            ConsoleKeyInfo key;
+            Console.WriteLine("Если хочешь попробовать еще нажми Enter");
+            Console.WriteLine();
+
+            Console.WriteLine("Для выхода из программы нажми Escape");
+            Console.WriteLine();
+
+            key = Console.ReadKey(); // повторение программы 
+            if (key.Key == ConsoleKey.Enter) goto again;
+
+        }
+    }
+}
