4b3b48b533b6d41952cbbe9f957d820e9f93a494
gaaasik <32246971+gaaasik@users.noreply.github.com>
2019-07-06 21:15:03 +0300

    Add files via upload

---
+++ task01/1-12/1-12/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   again:
+            Console.Clear();
+            string string1 = "";
+            string string2 = "";
+            string endsting = "";
+
+            Console.WriteLine("Введите первую строку:");
+            string1 = Console.ReadLine();
+
+            Console.WriteLine("Введите вторую строку:");
+            string2 = Console.ReadLine();
+
+            foreach (char symbol in string1)
+                if (!string2.Contains(symbol))
+                    endsting += symbol;
+                else
+                {
+                    endsting += symbol;
+                    endsting += symbol;
+                }
+            Console.WriteLine("Полученная  строка = " + endsting);
+            ConsoleKeyInfo key;
+            Console.WriteLine("Если хочешь попробовать еще нажми Enter");
+            Console.WriteLine();
+
+            Console.WriteLine("Для выхода из программы нажми Escape");
+            Console.WriteLine();
+
+            key = Console.ReadKey(); // повторение программы 
+            if (key.Key == ConsoleKey.Enter) goto again;
+          
+        }
+    }
+}
