fbd0de89a6db6d5cce38a239a4321ba9f0a94a11
gaaasik <32246971+gaaasik@users.noreply.github.com>
2019-07-25 10:53:04 +0300

    3-2

---
+++ task3/3-2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_2
+{
+    class Program
+    {
+        static Dictionary<string, int> Frequency(string str)
+        {
+            string[] words = str.Split(new char[] { ' ', '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> frequency = new Dictionary<string, int>();
+
+            foreach (string word in words )
+            {
+
+                if (frequency.ContainsKey(word.ToLower()))
+                    frequency[word.ToLower()]++;
+                else frequency.Add(word.ToLower(), 1);
+            }
+
+            return frequency;
+         }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст : ");
+            string str = Console.ReadLine();
+
+            Dictionary<string, int> frequency = Frequency(str);
+            foreach (string word in frequency.Keys)
+            {
+                Console.WriteLine(word + " - Встречается " + frequency[word] + " раз(а)");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
