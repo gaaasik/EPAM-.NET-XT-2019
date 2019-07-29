68fc306a501f29d98b2039189924fab056099407
gaaasik <32246971+gaaasik@users.noreply.github.com>
2019-07-29 19:47:44 +0300

    add 4-5

---
+++ Task04/4.5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _4._5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            var myString = Console.ReadLine();
+
+            if (myString.IsInt())
+            {
+                Console.WriteLine($"{myString} is positive integer");
+            }
+            else
+            {
+                Console.WriteLine($"{myString} is NOT positive integer");
+            }
+            Console.ReadKey();
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static bool IsInt(this string str)
+        {
+            if (str == null || str=="")
+            {
+                throw new ArgumentNullException($"Введите корректные значения {nameof(str)} !!");
+            }
+
+            str = str.Trim();
+
+            if (str.Length == 1 & str[0] == '0')
+            {
+                return false;
+            }
+
+            foreach (char symbol in str)
+            {
+                if (!char.IsDigit(symbol))  
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+       
+    }
+}
