921deca22010e337b083ae06e0067f67a48a36c3
gaaasik <32246971+gaaasik@users.noreply.github.com>
2019-08-21 20:27:28 +0300

    забыл загрузить на гит вовремя(

---
+++ Task_06_Design_Patterns/Users and Awards.BLL/DataManager.cs
@@ -0,0 +1,134 @@
+﻿using Users_and_Awards.Common;
+using Users_and_Awards.Entities;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Users_and_Awards.BLL
+{
+    public static class DataManager
+    {
+        private static IAwardRepository AwardsStorage => Dependencies.AwardsStorage;
+        private static IUserRepository UsersStorage => Dependencies.UsersStorage;
+        private static IAwardUserRepository AwardUserStorage => Dependencies.AwardUsersStorage;
+        private static ISaved SaveToFile => Dependencies.SaveToFile;
+
+        public static bool AddAwardToUser(string userName, string awardTitle)
+        {
+            Award award = AwardsStorage.GetAward(awardTitle);
+            User user = UsersStorage.GetUser(userName);
+
+            if (award != null && user != null)
+            {
+                if (AwardUserStorage.AddAwardUser(
+                    new AwardUser
+                    {
+                        AwardId = award.Id,
+                        Award = award,
+                        UserId = user.Id,
+                        User = user
+                    }))
+                {
+                    user.Awards.Add(award);
+                    award.Users.Add(user);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool AddAward(string title)
+        {
+            return AwardsStorage.AddAward(
+                         new Award
+                         {
+                             Title = title
+                         });
+        }
+
+        public static bool AddUser(string name, DateTime dateOfBirth)
+        {
+            return UsersStorage.AddUser(
+                        new User
+                        {
+                            Name = name,
+                            DateOfBirth = dateOfBirth
+                        });
+        }
+
+        public static IEnumerable<Award> GetAllAwards()
+        {
+            return AwardsStorage.GetAllAwards();
+        }
+
+        public static IEnumerable<User> GetAllUsers()
+        {
+            return UsersStorage.GetAllUsers();
+        }
+
+        public static bool RemoveAward(string title)
+        {
+            Award award = AwardsStorage.GetAward(title);
+
+            if (award != null && AwardsStorage.RemoveAward(title))
+            {
+                var users = UsersStorage.GetAllUsers()
+                                        .Where(u => u.Awards.Contains(award));
+
+                foreach (User user in users)
+                {
+                    user.Awards.Remove(award);
+                    AwardUserStorage.RemoveAwardUser(award.Id.ToString(), user.Id.ToString());
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool RemoveUser(string name)
+        {
+            User user = UsersStorage.GetUser(name);
+
+            if (user != null && UsersStorage.RemoveUser(name))
+            {
+                var awards = AwardsStorage.GetAllAwards()
+                                          .Where(a => a.Users.Contains(user));
+
+                foreach (Award award in awards)
+                {
+                    award.Users.Remove(user);
+                    AwardUserStorage.RemoveAwardUser(award.Id.ToString(), user.Id.ToString());
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool RemoveAwardToUser(string userName, string awardTitle)
+        {
+            Award award = AwardsStorage.GetAward(awardTitle);
+            User user = UsersStorage.GetUser(userName);
+
+            if (award != null && user != null)
+            {
+                if (AwardUserStorage.RemoveAwardUser(award.Id.ToString(), user.Id.ToString()))
+                {
+                    user.Awards.Remove(award);
+                    award.Users.Remove(user);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static void Save() => SaveToFile.Save();
+    }
+}
---
+++ Task_06_Design_Patterns/Users and Awards.Common/Dependencies.cs
@@ -0,0 +1,23 @@
+﻿using Users_and_Awards.DAL;
+using Users_and_Awards.Entities;
+
+namespace Users_and_Awards.Common
+{
+    public static class Dependencies
+    {
+        static Dependencies()
+        {
+            var data = new DataStorage();
+
+            AwardsStorage = data.AwardsStorage;
+            AwardUsersStorage = data.AwardUsersStorage;
+            SaveToFile = data;
+            UsersStorage = data.UsersStorage;
+        }
+
+        public static IAwardRepository AwardsStorage { get; }
+        public static IUserRepository UsersStorage { get; }
+        public static IAwardUserRepository AwardUsersStorage { get; set; }
+        public static ISaved SaveToFile { get; set; }
+    }
+}
